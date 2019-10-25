9a271308e706a23059b12329339a1e0e1ba059be
secondriver <413733843@qq.com>
2019-10-25 23:28:19 +0800

    leetcode

---
+++ project/tenToSixteen.java
@@ -0,0 +1,50 @@
+package D1;
+
+/**
+ * @author: Dennis
+ * @date: 2019/10/24 22:28
+ */
+
+/*
+给定一个整数，编写一个算法将这个数转换为十六进制数。对于负整数，我们通常使用 补码运算 方法。
+
+注意:
+十六进制中所有字母(a-f)都必须是小写。
+十六进制字符串中不能包含多余的前导零。如果要转化的数为0，那么以单个字符'0'来表示；对于其他情况，十六进制字符串中的第一个字符将不会是0字符。 
+给定的数确保在32位有符号整数范围内。
+不能使用任何由库提供的将数字直接转换或格式化为十六进制的方法。
+
+示例 1：
+输入:
+26
+输出:
+"1a"
+
+示例 2：
+输入:
+-1
+输出:
+"ffffffff"
+
+ */
+public class tenToSixteen {
+
+    public static String toHex(int num) {
+        char[] hex = "0123456789abcdef".toCharArray();
+        String s = new String();
+        while (num!=0){
+            int end = num&15;       //26 - 11010
+            s = hex[end] + s;       //15 - 01111
+            num = num >>4;
+        }
+        if (s.length() == 0){
+            s = "0";
+        }
+        return s;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(toHex(26));
+    }
+}
