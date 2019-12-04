207d80dbb3eb18619b6b9da62e8d1a4df13470ce
secondriver <413733843@qq.com>
2019-12-04 23:50:35 +0800

    reverseOnlyLetter

--- String/reverseOnlyLetter.java
+++ String/reverseOnlyLetter.java
@@ -47,11 +47,31 @@ class Solution {
         return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z';
     }
 }
-public class split {
+
+class Solution2 {
+    public static String reverseOnlyLetters(String S) {
+        StringBuilder ans = new StringBuilder();
+        int j = S.length() - 1;
+        for (int i = 0; i < S.length(); ++i) {
+            if (Character.isLetter(S.charAt(i))) {
+                while (!Character.isLetter(S.charAt(j)))
+                    j--;
+                ans.append(S.charAt(j--));
+            } else {
+                ans.append(S.charAt(i));
+            }
+        }
+
+        return ans.toString();
+    }
+}
+
+public class reverseOnlyLetter {
     public static void main(String[] args) {
         String string = new String();
         string = "abc-cd";
         System.out.println(Solution.reverseOnlyLetters(string));
+        System.out.println(Solution2.reverseOnlyLetters(string));
 
     }
 }
