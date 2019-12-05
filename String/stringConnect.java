2cbcef9e57a3a3a7db70eed4afe0ad400146f030
secondriver <413733843@qq.com>
2019-12-05 23:52:43 +0800

    connect

---
+++ String/stringConnect.java
@@ -0,0 +1,42 @@
+package String;
+
+import java.util.Scanner;
+
+/**
+ * @author: Dennis
+ * @date: 2019/12/1 10:22
+ */
+
+/*
+字符串无冗余拼接
+asd fgh
+asdfgh
+ */
+
+
+// class Demo {
+//    public static String func(String str){
+//        String[] strs = str.split(" ");
+//        String ret = "";
+//        for(String s : strs){
+//            ret += s ;
+//        }
+//        return ret;
+//    }
+// }
+
+public class stringConnect {
+//        public static void main(String[] args){
+//            Scanner scanner = new Scanner(System.in);
+//            String str = scanner.nextLine();
+//            String ret = Demo.func(str);
+//            System.out.println(ret);
+//        }
+
+        public static void main(String[] args){
+            Scanner scaner = new Scanner(System.in);
+            String string = scaner.nextLine();
+            System.out.println(string.replace(" ",""));
+        }
+    }
+
