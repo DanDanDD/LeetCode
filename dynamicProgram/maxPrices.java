3912fae18f776513f824e9ede39ca3602c06b3ec
secondriver <413733843@qq.com>
2020-04-24 23:58:45 +0800

    dynamicProgram

---
+++ dynamicProgram/maxPrices.java
@@ -0,0 +1,20 @@
+package dynamicProgram;
+
+/**
+ * @author: Dennis
+ * @date: 2019/11/26 23:01
+ */
+
+public class maxPrices {
+    public int maxProfit(int[] prices) {
+        int max = 0;
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                if (max <= (prices[j] - prices[i])) {
+                    max = (prices[j] - prices[i]);
+                }
+            }
+        }
+        return max;
+    }
+}
